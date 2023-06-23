c6e645f65e1f1fa25219f541819da2e7009a888e
Nathaniel Nosa <nathanielnarth@gmail.com>
2023-06-23 12:38:51 +0100

    new ip

--- uptechcommerce/settings.py
+++ uptechcommerce/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY  = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['16.170.225.155','localhost','*']
 
 
 # Application definition
